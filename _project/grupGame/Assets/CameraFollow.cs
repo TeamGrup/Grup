48ecd22ba3f5eda23d56d9e810cd9e7992a9c506
erica-fer <ericafer@uw.edu>
2020-05-12 16:04:38 -0700

    made camera movement be bounded. now should stop when approaching a wall

--- _project/grupGame/Assets/CameraFollow.cs
+++ _project/grupGame/Assets/CameraFollow.cs
@@ -7,10 +7,16 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
+
+
     void FixedUpdate()
     {
         Vector3 desiredPosition = targetToFollow.position + offset;
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        Vector3 wallHit = new Vector3(Mathf.Clamp(desiredPosition.x,-3f,3f)
+                                        ,Mathf.Clamp(desiredPosition.y,-4f,4f)
+                                        ,desiredPosition.z);
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, wallHit, smoothSpeed);
+        
         if(Input.GetAxis("Vertical") < 0 && Input.GetAxis("Horizontal") == 0)
         {
             Vector3 lookDownPosition = new Vector3(0,-2,-.3f);
@@ -28,6 +34,5 @@ public class CameraFollow : MonoBehaviour
         }
 
         transform.position = smoothPosition;
-
     }
 }
