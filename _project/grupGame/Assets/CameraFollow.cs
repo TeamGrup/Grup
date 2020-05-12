6c44adaea5fa6c91e0cc9d51abb8a24b31cc80ce
erica-fer <ericafer@uw.edu>
2020-05-12 11:50:11 -0700

    added camera follow + character can now look up&down

---
+++ _project/grupGame/Assets/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform targetToFollow;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = targetToFollow.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        if(Input.GetAxis("Vertical") < 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            Vector3 lookDownPosition = new Vector3(0,-2,-.3f);
+            desiredPosition = targetToFollow.position + lookDownPosition;
+            smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothPosition;
+        }
+
+        if(Input.GetAxis("Vertical") > 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            Vector3 lookDownPosition = new Vector3(0, 4, -.3f);
+            desiredPosition = targetToFollow.position + lookDownPosition;
+            smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothPosition;
+        }
+
+        transform.position = smoothPosition;
+
+    }
+}
