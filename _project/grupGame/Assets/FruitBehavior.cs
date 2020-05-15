d9000cee1e4f35c1a6e1941c87aa22bda21a5303
erica-fer <ericafer@uw.edu>
2020-05-14 17:02:28 -0700

    plant pickin is working

--- _project/grupGame/Assets/FruitBehavior.cs
+++ _project/grupGame/Assets/FruitBehavior.cs
@@ -13,25 +13,73 @@ public class FruitBehavior : MonoBehaviour
 
     public Vector3 position = new Vector3(1.3f,0,0);
 
+    public float CharacterFace;
+
+    private float timer;
+
     // Start is called before the first frame update
     void Start()
     {
         IsHeld = true;
         rb = gameObject.GetComponent<Rigidbody2D>();
         rb.isKinematic = true;
+        
+        col = gameObject.GetComponent<BoxCollider2D>();
+        col.enabled = false;
 
 
         player = GameObject.Find("playerCub");
+        CharacterFace = player.transform.right.x;
 
     }
 
     // Update is called once per frame
     void Update()
-    {       
+    {   
+        if(!IsHeld)
+        {
+            timer += Time.deltaTime;
+
+            // 5 / scale
+            //  (timer / 5) / scale
+
+
+
+            Vector3 scale = transform.localScale;
+            Debug.Log((timer/5)/scale.x);
+
+            // scale.x = scale.x/timer;
+            // scale.y = scale.y/timer;
+            // transform.localScale = scale;
+
+            if(timer >= 5)
+            {
+                Debug.Log("5 seconds");
+                player.GetComponent<FruitPick>().ObjectInWorld = null;
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(IsHeld)
+        {   
+            SetPosition();
+        }
+    }
+
+    void SetPosition()
+    {
         var playerFacing = player.transform.right;
         var playerPosition = player.transform.position;
         transform.right = playerFacing;
 
+            if(CharacterFace != player.transform.right.x)
+            {
+                CharacterFace = player.transform.right.x;
+                position = position * -1;
+            }
 
         transform.position = playerPosition + position;
     }
--- _project/grupGame/Assets/FruitPick.cs
+++ _project/grupGame/Assets/FruitPick.cs
@@ -5,26 +5,76 @@ using UnityEngine;
 public class FruitPick : MonoBehaviour
 {
     public GameObject prefabToSpawn;
+    public GameObject HeldObject;
 
-    void OnTriggerStay2D(Collider2D Collider)
+    public GameObject ObjectInWorld;
+
+    public bool CanCollect = false;
+
+
+    void LateUpdate()
+    {
+        if(Input.GetKeyDown("e"))
+        {
+            if(CanCollect && (HeldObject == null))
+            {
+                PickFruit();
+                CanCollect = false;
+            }else if(HeldObject != null)
+            {
+                DropFruit();
+            }else if((ObjectInWorld != null) && Vector3.Distance(ObjectInWorld.transform.position, transform.position) <= 2)
+            {
+                PickBackUp();
+            }
+            
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D Collider)
+    {
+        var CollidedWith = Collider.gameObject.tag;
+
+        if(CollidedWith == "FruitTree" && !ObjectInWorld)
+        {
+            CanCollect = true;          
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D Collider)
     {
         var CollidedWith = Collider.gameObject.tag;
 
         if(CollidedWith == "FruitTree")
         {
-            if(Input.GetKeyDown("e"))
-            {
-                PickFruit();
-            }
+            CanCollect = false;          
         }
     }
 
     void PickFruit()
     {
-        Debug.Log("Picking fruit");
         Vector3 fruitPosition = new Vector3(1.3f,0,0);
 
-        Instantiate(prefabToSpawn, gameObject.transform.position + fruitPosition, Quaternion.identity);
+        HeldObject = Instantiate(prefabToSpawn, gameObject.transform.position + fruitPosition, Quaternion.identity);
+        ObjectInWorld = HeldObject;
+        HeldObject.GetComponent<FruitBehavior>().IsHeld = true;
+    }
 
+    void DropFruit()
+    {
+            HeldObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            HeldObject.GetComponent<BoxCollider2D>().enabled = true;
+            HeldObject.GetComponent<FruitBehavior>().IsHeld = false;
+            HeldObject = null;
+    }
+
+    void PickBackUp()
+    {
+        HeldObject = ObjectInWorld;
+        HeldObject.GetComponent<FruitBehavior>().IsHeld = true;
+        HeldObject.GetComponent<Rigidbody2D>().isKinematic = true;
+        HeldObject.GetComponent<BoxCollider2D>().enabled = false;
+        // HeldObject.GetComponent<FruitBehavior>().IsHeld = true;
+        // ? implement picking fruit back up once dropped
     }
 }
