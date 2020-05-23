a72a4006d67ccda8634aa2c323ad252b6aa4d28d
Scott <scottin3d@gmail.com>
2020-05-23 16:56:51 -0700

    worked on menus, working on an audio manager

---
+++ GrupGame/Assets/ButtonClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+  public AudioSource soundFX;
+  public AudioClip buttonSelect;
+  public AudioClip buttonHighlight;
+
+  public void HoverSound() {
+    soundFX.PlayOneShot(buttonHighlight);
+  }
+  public void ClickSound() {
+    soundFX.PlayOneShot(buttonSelect);
+  }
+}
