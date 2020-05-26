729fde2a5c37ad85c74b3eb4f5bff3f29f7fc50d
erica-fer <ericafer@uw.edu>
2020-05-25 23:25:07 -0700

    updated conditions for walk sound. now they will not play when character is in air

--- GrupGame/Assets/WalkSoundBehavior.cs
+++ GrupGame/Assets/WalkSoundBehavior.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class WalkSoundBehavior : MonoBehaviour
 {
     private AudioSource walkAudio;
+    private player parentScript;
 
     // Start is called before the first frame update
     void Start()
     {
         walkAudio = this.GetComponent<AudioSource>();
+        parentScript = gameObject.GetComponentInParent<player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Mathf.Abs(Input.GetAxis("Horizontal")) > .5)
+        if(Mathf.Abs(Input.GetAxis("Horizontal")) > .5 && parentScript.onGround == true)
         {
             if(!walkAudio.isPlaying)
                 walkAudio.PlayDelayed(.15f);
