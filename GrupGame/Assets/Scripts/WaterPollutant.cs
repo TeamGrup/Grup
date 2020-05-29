78475cf6c99f8f67883a10b2608b11a9bc954f48
Ed Abshire <thaldin@comcast.net>
2020-05-28 21:55:02 -0700

    Pollutants

---
+++ GrupGame/Assets/Scripts/WaterPollutant.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class WaterPollutant : PollutantBehavior
+{
+    [Header("Movement")]
+    public float RotationSpeed = 1.0f;
+
+    //FLOAT CODE SOURCE:
+    //http://www.donovankeith.com/2016/05/making-objects-float-up-down-in-unity/
+    protected override void Float()
+    {
+        transform.Rotate(0, 0, RotationSpeed);
+    }
+}
