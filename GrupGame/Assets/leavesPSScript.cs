5bacb13e39d55c550d34ad1c2755e10b2620d920
Scott <scottin3d@gmail.com>
2020-06-05 12:55:14 -0700

    added kill script to leaves PS

---
+++ GrupGame/Assets/leavesPSScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Destroys particle system after XXX time
+/// </summary>
+public class leavesPSScript : MonoBehaviour {
+  public float aliveTime = 3f;
+  // Start is called before the first frame update
+  void Start() {
+    StartCoroutine(KillParticleSystem());
+  }
+
+
+  IEnumerator KillParticleSystem() {
+
+    yield return new WaitForSeconds(aliveTime);
+    Destroy(this);
+  }
+}
